2e5e1a4061dd4f65ce27aae3ed3440c26ac1dff5
Phils Garcia <eivanphils@gmail.com>
2016-02-23 23:08:05 -0430

    Validar contenido de titulo y url del post ante de insertar

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,6 +7,10 @@ Template.postSubmit.events({
             title:$(e.target).find('[name="title"]').val()
         };
 
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
+
         Meteor.call('postInsert', post, function(error, result) {
             // display the error to the user and abort
             if (error)
@@ -17,5 +21,18 @@ Template.postSubmit.events({
                 Router.go('postPage', {_id: result._id});
         });
 
+    }
+});
+
+Template.postSubmit.onCreated(function(){
+    Session.set('postSubmitErrors',{});
+});
+
+Template.postSubmit.helpers({
+    errorMessage: function(yield){
+        return Session.get('postSubmitErrors')[yield];
+    },
+    errorClass: function(yield){
+        return !!Session.get('postSubmitErrors')[yield] ? 'has-error' : '';
     }
 });
